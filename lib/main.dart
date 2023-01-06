9d46b3a5246f6e3b13d6a351e6b65431fc8399e6
Faiz Amanulloh <faizamanulloh@student.telkomuniversity.ac.id>
2023-01-06 12:15:47 +0700

    membuat template awal 1

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Text('home'),
+      home: const Text('home'),
     );
   }
 }
